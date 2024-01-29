2adf36633a23264c20cc56e3fe8f8623bbc2a49f
rhit-moej <moej@rose-hulman.edu>
2024-01-29 09:37:27 -0500

    added component skeletons

---
+++ src/mainApp/DataVisualizationComponent.java
@@ -0,0 +1,19 @@
+package mainApp;
+
+import javax.swing.JComponent;
+
+public class DataVisualizationComponent extends JComponent {
+
+	public void addEntry() {
+		
+	}
+	
+	public void reset() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+}
---
+++ src/mainApp/SimulatorComponent.java
@@ -0,0 +1,23 @@
+package mainApp;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class SimulatorComponent extends JComponent {
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+	}
+	
+	private void logDataInfo() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+}
