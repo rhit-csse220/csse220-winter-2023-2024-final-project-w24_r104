2637c2a40acc5ba51f3d4a8f088ed3fd3b830754
rhit-moej <moej@rose-hulman.edu>
2024-01-29 09:11:32 -0500

    created individual class

---
+++ src/mainApp/Individual.java
@@ -0,0 +1,23 @@
+package mainApp;
+
+public class Individual {
+
+	private int[] chromosome;
+	
+	public void calculateFitness() {
+		
+	}
+	
+	public void mutate() {
+		
+	}
+	
+	public void saveCurrentChromosome() {
+		
+	}
+	
+	public void loadChromosomeFromFile(String filename) {
+		
+	}
+	
+}
