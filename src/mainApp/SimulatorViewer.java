03b9f147ccd54069fbe42b746362f0cee54f3a27
Anh Ngo <qanhngo.cs@gmail.com>
2024-02-01 18:04:15 -0500

    Setup listener for save/load buttons

--- src/mainApp/SimulatorViewer.java
+++ src/mainApp/SimulatorViewer.java
@@ -3,6 +3,7 @@ package mainApp;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,11 +11,14 @@ import java.util.Scanner;
 import javax.imageio.stream.FileCacheImageInputStream;
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 
 /**
  * Class: MainApp
@@ -41,7 +45,7 @@ public class SimulatorViewer {
 		JTextField promptMRate = new JTextField(10);
 
 		JButton mutateButton = new JButton("Mutate");
-		mutateButton.addActionListener(new ActionListener() {	
+		mutateButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (!promptMRate.getText().equals("")) {
@@ -50,11 +54,6 @@ public class SimulatorViewer {
 				}
 			}
 		});
-		
-		JButton loadButton = new JButton("Load");
-		loadButton.addActionListener(new LoadListener());
-		JButton saveButton = new JButton("Save");
-		saveButton.addActionListener(new SaveListener());
 
 		Scanner s = new Scanner(System.in);
 
@@ -69,10 +68,10 @@ public class SimulatorViewer {
 				firstIndividual = simComp.initializePop(10, filename);
 				isDone = true;
 			} catch (InvalidChromosomeFormatException e) {
-				JOptionPane.showMessageDialog(panel, "Invalid file content", "ERROR", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(frame, "Invalid file content", "ERROR", JOptionPane.ERROR_MESSAGE);
 				System.err.println("Invalid file content: 0s and 1s only");
 			} catch (FileNotFoundException e) {
-				JOptionPane.showMessageDialog(panel, "File not found", "ERROR", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(frame, "File not found", "ERROR", JOptionPane.ERROR_MESSAGE);
 				System.err.println("File not found. Please try again.");
 
 			} catch (IOException e) {
@@ -80,6 +79,13 @@ public class SimulatorViewer {
 			}
 			s.close();
 		}
+
+		JButton loadButton = new JButton("Load");
+		JButton saveButton = new JButton("Save");
+
+		SaveLoadListener saveLoadListener = new SaveLoadListener(panel, chromosomeFileLabel);
+		loadButton.addActionListener(saveLoadListener);
+		saveButton.addActionListener(saveLoadListener);
 
 		frame.add(simComp, BorderLayout.CENTER);
 //		frame.add(new ChromosomeComponent(firstIndividual), BorderLayout.CENTER);
@@ -113,4 +119,28 @@ public class SimulatorViewer {
 		mainApp.runApp();
 	} // main
 
+	class SaveLoadListener implements ActionListener {
+		private JPanel panel;
+		private JLabel label;
+
+		public SaveLoadListener(JPanel panel, JLabel label) {
+			// TODO Auto-generated constructor stub
+			this.panel = panel;
+			this.label = label;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			String s = System.getProperty("user.dir");
+			JFileChooser chooser = new JFileChooser(s);
+//					FileNameExtensionFilter filter = new FileNameExtensionFilter("txt");
+//					chooser.setFileFilter(filter);
+			int result = chooser.showOpenDialog(this.panel);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = chooser.getSelectedFile();
+				this.label.setText(selectedFile.getAbsolutePath());
+			}
+		}
+	}
 }
